0c8aeb6d71b591099814e2a2d54669a7ed1ee689
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-14 07:58:07 +0530

    chore: Update devServer watch path

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    watchFiles: [path.resolve(__dirname, 'src/**/*.html')],
+    watchFiles: [path.resolve(__dirname, 'src/**/*')],
   },
   module: {
     rules: [
