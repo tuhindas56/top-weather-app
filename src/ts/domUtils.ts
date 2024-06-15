75240b8ae9e1dc95f4c9e86b2b39cd85586f4e5e
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-15 22:41:02 +0530

    feat: Add function to render current weather conditions to DOM

--- src/ts/domUtils.ts
+++ src/ts/domUtils.ts
@@ -4,6 +4,14 @@ const toggleUnitBtn = document.querySelector('.toggle-unit') as HTMLButtonElemen
 const searchBtn = document.querySelector('.search-btn') as HTMLButtonElement
 const todayCard = document.querySelector('.today-card') as HTMLDivElement
 const forecastCard = document.querySelector('.forecast-card') as HTMLDivElement
+const tempElement = document.querySelector('.temp') as HTMLParagraphElement
+const feelsLikeElement = document.querySelector('.feels-like') as HTMLParagraphElement
+const conditionElement = document.querySelector('.condition') as HTMLParagraphElement
+const dateElement = document.querySelector('.date') as HTMLParagraphElement
+const locationElement = document.querySelector('.location') as HTMLSpanElement
+const humidityElement = document.querySelector('.humidity') as HTMLParagraphElement
+const rainProbabilityElement = document.querySelector('.rain-probability') as HTMLSpanElement
+const currentIcon = document.querySelector('.current-icon') as HTMLImageElement
 
 export function renderError(error: Error) {
   errorElement.textContent = `Error: ${error.message}`
@@ -18,4 +26,25 @@ export function toggleLoader() {
   todayCard.classList.toggle('pulse')
   forecastCard.classList.toggle('pulse')
   loader.classList.toggle('none')
+}
+
+export function renderCurrentData(
+  unit: string,
+  temp: number,
+  feelsLike: number,
+  condition: string,
+  date: string,
+  location: { city: string; country: string },
+  humidity: number,
+  rainProbability: number,
+  iconPath: string,
+) {
+  currentIcon.src = iconPath
+  tempElement.innerHTML = `${temp}<sup>°${unit}</sup>`
+  feelsLikeElement.innerText = `Feels like: ${feelsLike}°`
+  conditionElement.innerText = condition
+  dateElement.innerText = date
+  locationElement.innerText = `${location.city}, ${location.country}`
+  humidityElement.innerText = `Humidity: ${humidity}%`
+  rainProbabilityElement.innerText = `Rain probability: ${rainProbability}%`
 }
