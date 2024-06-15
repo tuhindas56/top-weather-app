e938f981d23ff2297e20e028553611dd9bf172c3
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-15 22:54:52 +0530

    feat: Handle location fetch errors and provide a default

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,19 @@
 import '../style.css'
+import { fetchWeatherData, getIconPath, getLocation } from './data'
+import {
+  clearHourDataFromDOM,
+  renderCurrentData,
+  renderForecastData,
+  renderError,
+  renderHourData,
+  toggleLoader,
+} from './domUtils'
+
+async function locationHandler(location?: string) {
+  const response = await getLocation(location)
+  if (response instanceof Error) {
+    renderError(response)
+    return 'Tokyo'
+  }
+  return response
+}
