6bd202068a005fe2d6e397aa47bf6d91b9622559
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-15 22:38:08 +0530

    feat: Add function to toggle loading and disable input

--- src/ts/domUtils.ts
+++ src/ts/domUtils.ts
@@ -1,8 +1,21 @@
 const errorElement = document.querySelector('.error') as HTMLParagraphElement
+const loader = document.querySelector('.loader') as HTMLDivElement
+const toggleUnitBtn = document.querySelector('.toggle-unit') as HTMLButtonElement
+const searchBtn = document.querySelector('.search-btn') as HTMLButtonElement
+const todayCard = document.querySelector('.today-card') as HTMLDivElement
+const forecastCard = document.querySelector('.forecast-card') as HTMLDivElement
 
 export function renderError(error: Error) {
   errorElement.textContent = `Error: ${error.message}`
   setTimeout(() => {
     errorElement.textContent = ''
   }, 5000)
+}
+
+export function toggleLoader() {
+  toggleUnitBtn.disabled = !toggleUnitBtn.disabled
+  searchBtn.disabled = !searchBtn.disabled
+  todayCard.classList.toggle('pulse')
+  forecastCard.classList.toggle('pulse')
+  loader.classList.toggle('none')
 }
