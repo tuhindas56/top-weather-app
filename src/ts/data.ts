179bdb6c2bdfdc2f9fad49ab4d3c890de80dbf33
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-15 22:34:42 +0530

    feat: Import assets and add logic to return path based on condition code

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -2,6 +2,29 @@
 import { format } from 'date-fns'
 import { renderError } from './domUtils'
 
+// Import assets
+import clearDay from '../assets/svg/line/clear-day.svg'
+import clearNight from '../assets/svg/line/clear-night.svg'
+import cloudy from '../assets/svg/line/cloudy.svg'
+import cloudyDay from '../assets/svg/line/partly-cloudy-day.svg'
+import cloudyNight from '../assets/svg/line/partly-cloudy-night.svg'
+import drizzle from '../assets/svg/line/drizzle.svg'
+import fogDay from '../assets/svg/line/fog-day.svg'
+import fogNight from '../assets/svg/line/fog-night.svg'
+import overcastDay from '../assets/svg/line/overcast-day.svg'
+import overcastNight from '../assets/svg/line/overcast-night.svg'
+import mist from '../assets/svg/line/mist.svg'
+import notAvailable from '../assets/svg/line/not-available.svg'
+import partlyCloudyDayRain from '../assets/svg/line/partly-cloudy-day-rain.svg'
+import partlyCloudyNightRain from '../assets/svg/line/partly-cloudy-night-rain.svg'
+import rain from '../assets/svg/line/rain.svg'
+import sleet from '../assets/svg/line/sleet.svg'
+import snow from '../assets/svg/line/snow.svg'
+import thunderstormsDayRain from '../assets/svg/line/thunderstorms-day-rain.svg'
+import thunderstormsNightRain from '../assets/svg/line/thunderstorms-night-rain.svg'
+import thunderstormsDaySnow from '../assets/svg/line/thunderstorms-day-snow.svg'
+import thunderstormsNightSnow from '../assets/svg/line/thunderstorms-night-snow.svg'
+
 const API_KEY = 'e36def996b1a45bdb03142152241106'
 
 async function getCityFromIP() {
@@ -144,4 +167,103 @@ export async function fetchWeatherData(location: string) {
     renderError(error)
     return null
   }
+}
+
+export function getIconPath(code: number, time: string) {
+  let pathDay = ''
+  let pathNight = ''
+  switch (code) {
+    case 1000:
+      pathDay = clearDay
+      pathNight = clearNight
+      break
+    case 1003:
+      pathDay = cloudyDay
+      pathNight = cloudyNight
+      break
+    case 1006:
+      pathDay = cloudy
+      pathNight = cloudy
+      break
+    case 1009:
+      pathDay = overcastDay
+      pathNight = overcastNight
+      break
+    case 1030:
+      pathDay = mist
+      pathNight = mist
+      break
+    case 1063:
+      pathDay = partlyCloudyDayRain
+      pathNight = partlyCloudyNightRain
+      break
+    case 1180:
+    case 1183:
+    case 1186:
+    case 1189:
+    case 1192:
+    case 1195:
+    case 1198:
+    case 1201:
+    case 1240:
+    case 1243:
+    case 1246:
+      pathDay = rain
+      pathNight = rain
+      break
+    case 1069:
+    case 1204:
+    case 1207:
+    case 1237:
+    case 1249:
+    case 1252:
+    case 1261:
+    case 1264:
+      pathDay = sleet
+      pathNight = sleet
+      break
+    case 1087:
+    case 1273:
+    case 1276:
+      pathDay = thunderstormsDayRain
+      pathNight = thunderstormsNightRain
+      break
+    case 1066:
+    case 1114:
+    case 1117:
+    case 1210:
+    case 1213:
+    case 1216:
+    case 1219:
+    case 1222:
+    case 1225:
+    case 1255:
+    case 1258:
+      pathDay = snow
+      pathNight = snow
+      break
+    case 1135:
+    case 1147:
+      pathDay = fogDay
+      pathNight = fogNight
+      break
+    case 1072:
+    case 1150:
+    case 1153:
+    case 1168:
+    case 1171:
+      pathDay = drizzle
+      pathNight = drizzle
+      break
+    case 1279:
+    case 1282:
+      pathDay = thunderstormsDaySnow
+      pathNight = thunderstormsNightSnow
+      break
+    default:
+      pathDay = notAvailable
+      pathNight = notAvailable
+      break
+  }
+  return +time < 18 && +time > 4 ? pathDay : pathNight
 }
